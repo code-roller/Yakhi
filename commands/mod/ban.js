20a0d60c88c2a4bc575a05400e836be28d76b7d2
SansNeal <62467577+SansNeal@users.noreply.github.com>
2021-04-11 14:06:42 +0530

    Add files via upload

---
+++ commands/mod/ban.js
@@ -0,0 +1,18 @@
+module.exports.run = function(args, message, yakhi) {
+    if (message.member.hasPermission('ADMINISTRATOR') || message.member.hasPermission('BAN_MEMBERS')){
+        const target = message.mentions.users.first()
+        if (target){
+            const targetMember = message.mentions.guild.members.cache.get(target.id)
+            if (target.id === targetMember){
+                return message.channel.send(`<@${message.author.id}> Please specify someone in the server to ban.`)
+            }else{
+                targetMember.ban()
+                return message.channel.send(`<@${target}> Has been banned.`)
+            }
+        }else{
+            return message.channel.send(`<@${message.author.id}> Please specify someone to ban.`)
+        }
+    }else{
+        return message.channel.send(`<@${message.author.id}> You do not have permission to use this command.`)
+    }
+}
