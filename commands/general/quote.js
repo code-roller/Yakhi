d2569539285b8b5d708c922555dc123b9a65a112
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-05-07 14:25:21 +0530

    Quote

---
+++ commands/general/quote.js
@@ -0,0 +1,23 @@
+const axios = require('axios')
+const { MessageEmbed } = require('discord.js');
+const { messageEmbed } = require('../github/user');
+
+const run = (args, message, client) => {
+    axios.get('https://api.quotable.io/random')
+          .then((response) => {
+            const data = response.data;
+            const embed = new MessageEmbed()
+              .setColor('#9147ff')
+              .setDescription(`>>> ${data.content}`)
+              .addField(':pencil: Author', data.author, true);
+            message.channel.send(embed);
+          })
+          .catch((err) => {
+            const error = messageEmbed(
+                "Error :rolling_eyes:",
+              'An error occured while fetching quotes for you',
+            )
+          });
+}
+
+module.exports = { run }
