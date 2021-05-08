4f431c1a4de99358981dc80786533e82aab6d825
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-05-08 20:53:54 +0530

    dev.to top user

---
+++ commands/dev/top.js
@@ -0,0 +1,36 @@
+// @ts-check
+
+const { default: axios } = require("axios")
+const { MessageEmbed } = require("discord.js")
+const { messageEmbed } = require('../github/user.js')
+
+function articles(message){
+    /** @type {string} */
+    const api = "https://dev.to/api/articles?top=1"
+    axios.get(api).then((response) => {
+        const data = response.data[0]
+        const embed = new MessageEmbed()
+        embed.setTitle(data.title)
+        embed.setDescription(data.description)
+        embed.setURL(data.url)
+        embed.addField(":speech_balloon: Comments", data.comments_count, true)
+        embed.addField(":calendar_spiral: Published on", data.readable_publish_date, true)
+        embed.addField(":heart: Reactions", data.positive_reactions_count, true)
+        embed.setImage(data.cover_image)
+        embed.addField(":clock: Reading time", `${data.reading_time_minutes} minutes`, true)
+        embed.addField(":pencil: Author", `@${data.user.username}`, true)
+        message.channel.send(embed).then((sendMessage) => {
+        }).catch((error) => {
+        })
+    }).catch((error) => {
+        const embed = messageEmbed(
+            "An error occured",
+            "Failed to fetch articled for you"
+        )
+        message.channel.send(embed)
+    })
+}
+
+module.exports.run = function(args, message, client) {
+    articles(message)
+}
