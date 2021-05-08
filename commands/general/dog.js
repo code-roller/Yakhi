9fd01966e324a932ade86f64d2bdd959227cf8b6
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-05-08 12:27:00 +0530

    Random dog images

---
+++ commands/general/dog.js
@@ -0,0 +1,22 @@
+const axios = require('axios')
+const { MessageEmbed } = require('discord.js')
+const { getRandomColor } = require('../../utils/embeds.js')
+
+const API = "https://api.thedogapi.com/v1/images/search"
+
+module.exports.run = function (args, message, client) {
+    axios.default.get(API).then((response) => {
+        const data = response.data[0]
+        const embed = new MessageEmbed()
+        embed.setColor(getRandomColor())
+        embed.setImage(data.url)
+        message.channel.send(embed)
+
+    }).catch((exception) => {
+        const embed = new MessageEmbed()
+        embed.setColor("#ED010E")
+        embed.setTitle("An unexpected error occured")
+        embed.setDescription("Unable to fetch data for you")
+        message.channel.send(embed)
+    })
+}
